c720bc14527f5573da71cbfeecee35165cddd445
Daniel Kenji <90214635+kenjiodan@users.noreply.github.com>
2025-02-18 16:31:51 -0300

    javascript update

---
+++ cursoemvideo/exercise/ex006/index.js
@@ -0,0 +1,10 @@
+
+var pais = 'Canadá'
+
+console.log(`Cidadão do ${pais}`)
+
+if(pais == 'Brasil') { //Condição Simples
+    console.log(`Olá, Brasileiro!`)
+}
+
+console.log('Seja bem-vindo!')
---
+++ cursoemvideo/exercise/ex006/index2.js
@@ -0,0 +1,10 @@
+var vel = 70
+
+console.log(`A velocidade do seu veículo está em ${vel}km/h.`)
+
+if ( vel > 60) { //Condição Composta
+    console.log('Parado ai! Você foi multado por andar em alta velocidade!')
+}
+else {
+    console.log('Tenha uma ótima viagem!')
+}
