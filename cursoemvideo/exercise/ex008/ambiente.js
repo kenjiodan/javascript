21918ba66693ecc022b892936c6514e3ea018a72
Daniel Kenji <90214635+kenjiodan@users.noreply.github.com>
2025-03-01 03:52:15 -0300

    laço de repetição pt2

--- cursoemvideo/exercise/ex008/ambiente.js
+++ cursoemvideo/exercise/ex008/ambiente.js
@@ -1,4 +1,4 @@
-var c = 1
+/* var c = 1
 while (c <= 10) { //Estrutura de Repetição com teste lógico no Inicio 
     console.log(`Passo ${c}`)
     c++
@@ -7,6 +7,12 @@ while (c <= 10) { //Estrutura de Repetição com teste lógico no Inicio
 do { //Estrutura de Repetição com teste lógico no Fim
     console.log(`Passo ${c}`)
     c++
-} while (c <= 10)
+} while (c <= 10) */
+
+//------------------------------
+
+for (var c = 1 ;c <= 10 ; c++) {
+    console.log(`Passo ${c}`)
+}
 
     
